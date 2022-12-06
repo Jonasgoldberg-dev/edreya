7433fe3d0bcccdf27fc1a4a4a690960004a1de3d
Jonas Goldberg <jonas.a.goldberg@gmail.com>
2022-12-06 15:46:11 +0100

    commit

---
+++ src/components/popup/AboutPopup.js
@@ -0,0 +1,230 @@
+import { useContext } from "react";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { context } from "../../context/context";
+import { testimonialSlider } from "../../sliderProps";
+import AnimatedText from "../AnimatedText";
+import Modal from "./Modal";
+
+const AboutPopup = ({ open, close, aboutData }) => {
+  const colorContext = useContext(context);
+  const { color } = colorContext;
+
+  return (
+    <Modal open={open} close={close}>
+      <div className="about_popup_details">
+        <div className="left">
+          <div className="left_inner">
+            <div className="author">
+              <div className="avatar_image">
+                <img src="img/thumbs/1-1.jpg" alt="" />
+                <div className="main" data-img-url="img/about/1.jpg" />
+              </div>
+              <div className="short">
+                <h3 className="name">
+                  {aboutData.firstName}{" "}
+                  <span className="coloring">{aboutData.lastName}</span>
+                </h3>
+                <h3 className="job">
+                  <AnimatedText />
+                </h3>
+              </div>
+            </div>
+            <div className="list">
+              <ul>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-user" />
+                    <span>
+                      {aboutData.firstName} {aboutData.lastName}
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-calendar" />
+                    <span>{aboutData.bithday}</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-location" />
+                    <span>
+                      <a href="#" className="href_location">
+                        {aboutData.address}
+                      </a>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-phone" />
+                    <span>
+                      <a href="#">{aboutData.phn}</a>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-mail-1" />
+                    <span>
+                      <a href={`mailto:${aboutData.email}`}>
+                        {aboutData.email}
+                      </a>
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+        <div className="right">
+          <div className="right_inner">
+            <div className="biography">
+              <div className="about_title">
+                <h3>
+                  <span>
+                    Om <span className="coloring">Mig</span>
+                  </span>
+                </h3>
+              </div>
+              <div className="text">
+                <p>
+                  Hej! Jeg hedder <span>Jonas Goldberg.</span> Jeg er selvstændig webudvilker og konsulent. <br></br> <br></br>
+                  Jeg har speciale i online B2B & B2C salg, og beskæftiger mig med alt hvad det indebærer, at drive en online forretning.<br></br> <br></br>
+                  Jeg har mere end 7 års erfaring med at udvikle og drive online forretninger, og har hjulpet mange virksomheder med at få succes online.
+                </p>
+              </div>
+            </div>
+            <div className="service">
+              <div className="about_title">
+                <h3>
+                  <span>
+                    Hvad jeg <span className="coloring">tilbyder</span>
+                  </span>
+                </h3>
+              </div>
+              <div className="list">
+                <ul>
+                  {aboutData.serviceLists &&
+                    aboutData.serviceLists.map((service, i) => (
+                      <li key={i}>
+                        <i className="icon-right-dir" />
+                        {service}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+            <div className="prog_skill">
+              <div className="about_title">
+                <h3>
+                  <span>
+                    Programmør  <span className="coloring">Skills</span>
+                  </span>
+                </h3>
+              </div>
+              <div className="oki_progress">
+                {aboutData.skills &&
+                  aboutData.skills.programming &&
+                  aboutData.skills.programming.map((programming, i) => (
+                    <div
+                      key={i}
+                      className="progress_inner skillsInner___"
+                      data-value={95}
+                    >
+                      <span>
+                        <span className="label">{programming.name}</span>
+                        <span
+                          className="number"
+                          style={{ right: `${100 - programming.value}%` }}
+                        >
+                          {programming.value}%
+                        </span>
+                      </span>
+                      <div className="background">
+                        <div className="bar open">
+                          <div
+                            className="bar_in"
+                            style={{ width: `${programming.value}%` }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="lang_skill">
+              <div className="about_title">
+                <h3>
+                  <span>
+                    Sprog <span className="coloring"></span>
+                  </span>
+                </h3>
+              </div>
+              <div className="circular_progress_bar">
+                <ul>
+                  {aboutData.skills &&
+                    aboutData.skills.language &&
+                    aboutData.skills.language.map((language, i) => (
+                      <li key={i}>
+                        <div className="list_inner">
+                          <div className="myCircle" data-value="0.95">
+                            <CircularProgressbar
+                              value={language.value}
+                              text={`${language.value}%`}
+                              strokeWidth={2}
+                              styles={buildStyles({
+                                // Colors
+                                pathColor: color,
+                              })}
+                            />
+                          </div>
+                          <div className="title">
+                            <span>{language.name}</span>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+            
+            <div className="timeline">
+              <div className="about_title">
+                <h3>
+                  <span>
+                    Arbejde <span className="coloring">Timeline</span>
+                  </span>
+                </h3>
+              </div>
+              <div className="list">
+                <ul>
+                  {aboutData &&
+                    aboutData.working &&
+                    aboutData.working.map((work, i) => (
+                      <li key={i}>
+                        <div className="list_inner">
+                          <div className="time">
+                            <span>{work.year}</span>
+                          </div>
+                          <div className="place">
+                            <h3>{work.company}</h3>
+                            <span>{work.deg}</span>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+            
+            
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+export default AboutPopup;
