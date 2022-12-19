a3137ef8b5940128b88a59055faa9f370eda789a
Jonas Goldberg <jonas.a.goldberg@gmail.com>
2022-12-19 14:31:24 +0100

    commit

--- .next/static/Zf4GrtfT5bQPclEGvqdEN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/89-496d83d439357f3d.js","static/chunks/pages/index-95526f73e3d186eb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
