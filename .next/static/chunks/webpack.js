6bac38582d1176029289cf3a15df64cc45a1824b
Jonas Goldberg <jonas.a.goldberg@gmail.com>
2022-12-19 16:57:34 +0100

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8234a0a1acdc8351"; }
+/******/ 		__webpack_require__.h = function() { return "e5cef66e9a783846"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
